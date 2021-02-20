66665734fa864026bce8d400c48807074ec87cd1
Bodo Tasche <bodo@wannawork.de>
2021-02-20 16:40:52 +0100

    Added currency symbol

--- routes/index.js
+++ routes/index.js
@@ -22,13 +22,14 @@ router.get('/:budgetId', async (req, res) => {
     return;
   }
   const budget = json.data.budget;
+  const currency = budget.currency_format.currency_symbol;
   const selectedCategories = getCategories(req.query.categories)
 
   const categories = budget.categories.filter(category => !selectedCategories || selectedCategories.includes(category.name))
                             .filter(category => !category.deleted)
                             .map(category => ({...category, budget_percentage: category.balance / ((category.activity * -1 + category.balance) / 100)}))
 
-  res.render('index', { categories });
+  res.render('index', { currency, categories });
 });
 
 router.get('/', async (req, res) => {
